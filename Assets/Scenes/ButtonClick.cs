28c995ead671a284b0058e9bed5e09aa88575bc1
ken6078 <ken60786213@gmail.com>
2022-06-22 14:15:59 +0800

    first commit

---
+++ Assets/Scenes/ButtonClick.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+    public void debugPrint(string message){
+        Debug.Log(message);
+    }
+}
