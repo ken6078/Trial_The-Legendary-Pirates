09e7dbd004458b26e6ecc3b10e237cd5216b75bf
ken6078 <ken60786213@gmail.com>
2022-06-23 19:03:18 +0800

    Fix Game Chart System

--- Assets/Scenes/TextShow.cs
+++ Assets/Scenes/TextShow.cs
@@ -6,31 +6,55 @@ using TMPro;
 
 public class TextShow : MonoBehaviour
 {
-    // Text mText;
-    // TextMesh mText;
-    [SerializeField] TextMeshProUGUI mText;
-    public List<string> testList = new List<string>();
-    public float typingSpeed = 0.2f;
+    //劇本
+    [SerializeField] TextAsset txt;
+    [SerializeField] TextMeshProUGUI NameTest;
+    [SerializeField] TextMeshProUGUI CharTest;
+    //UI
+    [SerializeField] GameObject reporter;
+    [SerializeField] GameObject judge;
+    [SerializeField] GameObject amber;
+    [SerializeField] GameObject jonny;
+
+    [SerializeField] List<string> testList = new List<string>();
+    [SerializeField] float typingSpeed = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
-        // mText = GetComponent<Text>();
-        // mText = gameObject.GetComponent<TextMesh>();
+        testList = new List<string>(txt.text.Split('\n'));
         StartCoroutine(Type());
-        
     }
 
     IEnumerator Type() {
         for (int index = 0; index < testList.Count; index++){
-            mText.text = "";
-            foreach (char letter in testList[index].ToCharArray()) {
-                mText.text += letter;
-                yield return new WaitForSeconds(typingSpeed);
+            //這一行是選項
+            if (testList[index][1] == '-'){
+                
             }
-            while(! Input.GetKeyDown (KeyCode.W))
-                yield return null;
-
-        }
-       
+            else{
+                string name = testList[index].Substring(0, testList[index].IndexOf(":"));
+                string chate = testList[index].Substring(testList[index].IndexOf(":")+1);
+                NameTest.text = name;
+                reporter.SetActive(false);
+                judge.SetActive(false);
+                amber.SetActive(false);
+                jonny.SetActive(false);
+                if (name == "記者")
+                    reporter.SetActive(true);
+                else if (name == "法官")
+                    judge.SetActive(true);
+                else if (name == "安柏赫德")
+                    amber.SetActive(true);
+                else
+                    jonny.SetActive(true);
+                CharTest.text = "";
+                foreach (char letter in chate.ToCharArray()) {
+                    CharTest.text += letter;
+                    yield return new WaitForSeconds(typingSpeed);
+                }
+                while(! Input.GetKeyDown(KeyCode.Space))
+                    yield return null;
+            }
+        } 
     }
 }
