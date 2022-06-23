9818e97178f60fafbfc76f35bf62ff0387e5c455
ken6078 <ken60786213@gmail.com>
2022-06-23 11:49:12 +0800

    Add Game Sense

--- Assets/Scenes/ButtonClick.cs
+++ Assets/Scenes/ButtonClick.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonClick : MonoBehaviour
 {
     public void debugPrint(string message){
         Debug.Log(message);
+    }
+    public void jumpScene(string id){
+        SceneManager.LoadScene(id);
     }
 }
