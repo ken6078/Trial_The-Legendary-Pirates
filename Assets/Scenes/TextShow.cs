493ab4498ce54b426a3ad4667edef9819e36a088
ken6078 <ken60786213@gmail.com>
2022-06-23 14:50:10 +0800

    Fix Chat System

--- Assets/Scenes/TextShow.cs
+++ Assets/Scenes/TextShow.cs
@@ -2,22 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TextShow : MonoBehaviour
 {
-    Text mText;
-    public string showStr = "今天天氣很好, \n我覺得我該寫一下程式 可是我不會寫程式";
+    // Text mText;
+    // TextMesh mText;
+    [SerializeField] TextMeshProUGUI mText;
+    public List<string> testList = new List<string>();
     public float typingSpeed = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
-        mText = GetComponent<Text>();
+        // mText = GetComponent<Text>();
+        // mText = gameObject.GetComponent<TextMesh>();
         StartCoroutine(Type());
+        
     }
+
     IEnumerator Type() {
-       foreach (char letter in showStr.ToCharArray()) {
-           mText.text += letter;
-           yield return new WaitForSeconds(typingSpeed);
-       }
+        for (int index = 0; index < testList.Count; index++){
+            mText.text = "";
+            foreach (char letter in testList[index].ToCharArray()) {
+                mText.text += letter;
+                yield return new WaitForSeconds(typingSpeed);
+            }
+            while(! Input.GetKeyDown (KeyCode.W))
+                yield return null;
+
+        }
+       
     }
 }
